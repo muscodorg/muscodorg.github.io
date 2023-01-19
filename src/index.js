1b9362b694bd91dc42c4e13b79d10f0a94876874
AKSHAT <114349022+codeproak@users.noreply.github.com>
2023-01-19 13:54:37 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function openhead() {
+    document.getElementById("headerlinks").style.display="block";
+    document.getElementById("headerlinks").style.opacity="100";
+}
+function closehead() {
+    document.getElementById("headerlinks").style.display="none";
+    document.getElementById("headerlinks").style.opacity="0";
+}
